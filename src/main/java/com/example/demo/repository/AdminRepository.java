e293aaa7fd6252596cbfbd2c324783e02675ccd4
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2026-04-07 11:41:11 -0300

    Update AdminRepository.java

--- src/main/java/com/example/demo/repository/AdminRepository.java
+++ src/main/java/com/example/demo/repository/AdminRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
-    Optional<Admin> findByEmail(String email); // O Spring cria a query mágica aqui
+    Optional<Admin> findByEmail(String email); 
 }
